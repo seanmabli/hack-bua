ef51ac7f14def67fd4c4d78c0e49fc9efb8ac3fc
laptop <laptop@Desktop-2Z2SQV2.hbcommunications.us>
2023-04-02 16:03:38 -0400

    Jagonious

--- languageprocessing/languageprocessing.py
+++ languageprocessing/languageprocessing.py
@@ -4,7 +4,33 @@ import speech_recognition as sr
 import pyttsx3
 import time
 
+from firebase_admin import credentials, initialize_app, storage
+
+
+cred = credentials.Certificate('patientmonitorinterface-firebase-adminsdk-fami0-7bbad24660.json')
+initialize_app(cred, {'storageBucket': 'patientmonitorinterface.appspot.com'})
+
+
+source_blob_name = "audio.webm"
+destination_file_name = r"C:\Users\laptop\hack-bua\languageprocessing\test.webm"
+bucket_name = "patientmonitorinterface.appspot.com"
+
+bucket = storage.bucket()
+blob = bucket.blob(source_blob_name)
+blob.download_to_filename(destination_file_name)
+
+
+# Python program to translate
+# speech to text and text to speech
+
+# Initialize the recognizer
 r = sr.Recognizer()
+
+# Function to convert text to
+# speech
+
+# Loop infinitely for user to
+# speak
 
 def checkInput(keywords, text):
 	for word in keywords:
@@ -12,21 +38,21 @@ def checkInput(keywords, text):
 			return True
 	return False
 
-def checkpatient():
-	keywords = ('help').split()
+def checkpatient(file):
+	keywords = ('help choke ouch choking choked hurt hurting hurts pain painful').split()
 	t = time.time()
 	# Exception handling to handle
 	# exceptions at the runtime
 	try:
 		
 		# use the microphone as source for input.
-		with sr.Microphone() as source2:
+		with sr.AudioFile(file) as source2:
 			print("start of mic: " + str(time.time()-t))
 			t = time.time()
 			# wait for a second to let the recognizer
 			# adjust the energy threshold based on
 			# the surrounding noise level
-			r.adjust_for_ambient_noise(source2, duration=1)
+			r.adjust_for_ambient_noise(source2, duration=0.5)
 			print("after ambient noise: " + str(time.time()-t))
 			t = time.time()
 			#listens for the user's input
@@ -48,4 +74,4 @@ def checkpatient():
 		print("Could not request results; {0}".format(e))
 		
 	except sr.UnknownValueError:
-		print("unknown error occurred")
+		return False, ''
