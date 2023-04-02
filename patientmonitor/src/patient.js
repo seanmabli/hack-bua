c5d06e5a2589cd6058d9698e088e3209ed86227b
Sean Mabli <smdrone1@gmail.com>
2023-04-02 11:44:33 -0400

    add webcam

--- patientmonitor/src/patient.js
+++ patientmonitor/src/patient.js
@@ -1,10 +1,13 @@
 import { Navbar } from "./navbar";
+import React from "react";
+import Webcam from "react-webcam";
 
 export function Patient() {
   return (
     <div>
       <Navbar />
       <p>Patient</p>
+      <Webcam />
     </div>
   );
 }
