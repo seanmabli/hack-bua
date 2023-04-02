2daf24cbf48113793cb0631f00925e88356233bd
Arjun Goray <goraya25@lsrhs.net>
2023-04-02 09:41:00 -0400

    added model folder

---
+++ patientmodel/train.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt # for plotting
+import numpy as np # for transformation
+
+import torch # PyTorch package
+import torchvision # load datasets
+import torchvision.transforms as transforms # transform data
+import torch.nn as nn # basic building block for neural neteorks
+import torch.nn.functional as F # import convolution functions like Relu
+import torch.optim as optim # optimzer
